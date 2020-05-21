5b6760ea8a236f50b508593bd6a07701457b517b
Vivek <vivekkhot1308@gmail.com>
2020-05-22 00:44:39 +0100

    First commit

---
+++ src/main/java/com/qa/mycode/DemoCode.java
@@ -0,0 +1,30 @@
+package com.qa.mycode;
+
+public class DemoCode {
+
+	public void add()
+	{
+		System.out.println("This is my Add method");
+	}
+	
+	public void subtract()
+	{
+		System.out.println("This is my subtract method");
+	}
+	
+	public void mul()
+	{
+		System.out.println("This is my multiplication method");
+	}
+	
+	public static void main(String[] args) {
+		DemoCode objDemo = new DemoCode();		
+		objDemo.add();
+		objDemo.subtract();
+		objDemo.mul();
+	}
+	
+	
+	
+
+}
---
+++ src/test/java/com/qa/testunit/DemoTest.java
@@ -0,0 +1,5 @@
+package com.qa.testunit;
+
+public class DemoTest {
+
+}
